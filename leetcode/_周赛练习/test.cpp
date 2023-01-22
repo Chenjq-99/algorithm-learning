cf595c1003b28950647cd2df4080c31e7ad6a4f8
chenjq <chenjq991212@gmail.com>
2023-01-22 10:27:08 +0800

    跟新周赛模板

--- leetcode/_周赛练习/test.cpp
+++ leetcode/_周赛练习/test.cpp
@@ -1,31 +1,8 @@
-#include<algorithm>
-#include<vector>
-#include<unordered_map>
-#include<unordered_map>
-#include<set>
-#include<map>
-#include<cstring>
-#include<numeric>
-#include<queue>
+#include<bits/stdc++.h>
 using namespace std;
 
 typedef long long LL;
-typedef pair<LL, LL> PLL;
 typedef pair<int, int> PII;
-typedef pair<double, double> PDD;
-typedef vector<int> VI;
-typedef vector<LL> VLL;
-typedef vector<double> VD;
-typedef vector<VI> VVI;
-typedef vector<VLL> VVLL;
-typedef vector<PII> VPII;
-typedef vector<PLL> VPLL;
-#define V vector
-#define pb push_back
-#define eb emplace_back
-
-#define _1th first
-#define _2nd second
-#define ALL(v) v.begin(),v.end()
 
 const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+
