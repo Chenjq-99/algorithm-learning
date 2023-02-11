a7014fc55002d4f9f498b05f82627560e0a791a6
chenjq <chenjq991212@gmail.com>
2023-02-11 09:02:40 +0800

    更新每日一题230211

--- leetcode/_每日一题/_2335_装满杯子需要的最短总时长.py
+++ leetcode/_每日一题/_2335_装满杯子需要的最短总时长.py
@@ -1,4 +1,5 @@
 # https://leetcode.cn/problems/minimum-amount-of-time-to-fill-cups
+import heapq
 class Solution:
     def fillCups(self, amount: list[int]) -> int:
         amount.sort()
@@ -6,3 +7,18 @@ class Solution:
             return amount[2]
         return (sum(amount) + 1) // 2
 
+# สนำรถั
+class Solution:
+    def fillCups(self, amount: list[int]) -> int:
+        amount = [-x for x in amount]
+        cnt = 0
+        heapq.heapify(amount)
+        a = heapq.heappop(amount)
+        b = heapq.heappop(amount)
+        while b:
+            cnt += 1
+            heapq.heappush(amount, a + 1)
+            heapq.heappush(amount,b + 1)
+            a = heapq.heappop(amount)
+            b = heapq.heappop(amount)
+        return cnt - a
