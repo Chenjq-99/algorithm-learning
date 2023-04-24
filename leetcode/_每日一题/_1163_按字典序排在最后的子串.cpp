3041b534e40f8346717a8f1d9257e903d0bc3c37
chenjq <chenjq991212@163.com>
2023-04-24 09:11:57 +0800

    add daily 230424

---
+++ leetcode/_每日一题/_1163_按字典序排在最后的子串.cpp
@@ -0,0 +1,23 @@
+#include<string>
+using namespace std;
+// https://leetcode.cn/problems/last-substring-in-lexicographical-order/description/
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int i = 0, j = 1, n = s.size();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s[i + k] == s[j + k]) {
+                k++;
+            }
+            if (j + k < n && s[i + k] < s[j + k]) {
+                int t = i;
+                i = j;
+                j = max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substr(i);
+    }
+};
