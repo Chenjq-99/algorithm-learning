9719c0dbe833d7fc0141890f4f28c9280dfa5c2a
chenJiaqi <chenjq991212@gmail.com>
2023-10-24 10:48:21 +0800

    add \CF\practice\_1443_C_The_Delivery_Dilemma.cpp

---
+++ CF/practice/_1443_C_The_Delivery_Dilemma.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+const int N = 200010;
+
+using pii = std::pair<int, int>;
+
+int n;
+pii a[N];
+
+void solve() {
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i].first;
+	}
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i].second;
+	}
+	std::sort(a, a + n);
+	int sum = 0, res = 0x3f3f3f3f;
+	for (int i = n - 1; i >= 0; i--) {
+		res = std::min(res, std::max(a[i].first, sum));
+		sum += a[i].second;
+	}
+	res = std::min(res, sum);
+	std::cout << res << std::endl;
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t; std::cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
